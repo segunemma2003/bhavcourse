cdfe8249781c0cb874faba551c7800a4fff55783
kutteai <kutteai978@gmail.com>
2025-05-16 09:57:39 +0100

    chnages

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = False
+# os.environ.get('DEBUG', 'False').lower() == 'true'
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
