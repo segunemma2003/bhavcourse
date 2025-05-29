a1b2266931c1e8233dadd5b6efea5dd0515bb881
kutteai <kutteai978@gmail.com>
2025-05-29 21:04:10 +0100

    chnages

--- core/admin_views.py
+++ core/admin_views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.db.models import Count, Sum
 from django.utils import timezone
+from django.db import transaction
 from datetime import timedelta, datetime
 from django.db.models.functions import TruncDate, TruncWeek, TruncMonth
 from .models import Course, CoursePlanType, Notification, PaymentCard, PaymentOrder, User, Purchase, ContentPage, GeneralSettings
