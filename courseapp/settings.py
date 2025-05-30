67f2bfe2d5f1d49ac2dcdeb8cf0fee461c20fb03
kutteai <kutteai978@gmail.com>
2025-05-30 06:45:25 +0100

    chnages

--- courseapp/settings.py
+++ courseapp/settings.py
@@ -306,7 +306,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '3306'),
+        'CONN_MAX_AGE': 60,  # 10 minutes
         'OPTIONS': {
+            'MAX_CONNECTIONS': 20,
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
